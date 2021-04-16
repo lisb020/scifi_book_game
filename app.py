d933d602b467599dc0f654adfa6bdc89aa66405b
Lisa Stroh <lisb020@yahoo.com>
2021-04-15 23:49:43 -0700

    subgenre on website

--- app.py
+++ app.py
@@ -9,17 +9,14 @@ from flask import (
 from keras.preprocessing.text import Tokenizer as kerasTokenizer
 from keras.preprocessing.sequence import pad_sequences
 import pickle
+import numpy as np
+
+winner = ""
 
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
-# model = load_model('rating_model')
-
-#create route that renders index.html template
-@app.route("/")
-def home():
-    return render_template("index.html")
 
 def remove_nums(row):
   output = ''.join(c for c in str(row) if not c.isdigit())
@@ -33,9 +30,19 @@ def remove_unicode(row):
   string_encode = str(row).encode("ascii", "ignore")
   return(string_encode.decode())
 
+
+#create route that renders index.html template
+@app.route("/", methods=["GET", "POST"])
+def home():
+    global winner
+    return render_template("index.html", subgenre=winner)
+
+
+
 # Send the user inputted data to the saved model
 @app.route("/send", methods=["GET", "POST"])
 def send():
+    global winner
     if request.method == "POST":
         #bring in user input
         data = request.get_json()
@@ -46,19 +53,27 @@ def send():
         data = remove_unicode(data)
         data = [data]
         
-        # with open('tokenizer.pickle', 'rb') as handle:
-        #     tokenizer = pickle.load(handle)
-        #     seq = tokenizer.texts_to_sequences(data)
-        #     padded = pad_sequences(seq, maxlen=250)
+        with open('tokenizer.pickle', 'rb') as handle:
+            tokenizer = pickle.load(handle)
+            seq = tokenizer.texts_to_sequences(data)
+            padded = pad_sequences(seq, maxlen=250)
+            print("successfully loaded tokenizer ", padded)
 
         # Load the model
         from tensorflow.keras.models import load_model
         subgenre_model = load_model("subgenre_trained.h5")
         print("successfully loaded file")
-        # model_loss, model_accuracy = rating_model.evaluate(description)
+        
+        pred = subgenre_model.predict(padded)
+        labels = ['sf_aliens', 'sf_alternate_history', 'sf_alternate_universe','sf_apocalyptic', 'sf_cyberpunk', 'sf_dystopia', 'sf_hard','sf_military', 'sf_robots', 'sf_space_opera', 'sf_steampunk','sf_time_travel']
+        winner = labels[np.argmax(pred)]
+        print("prediction: " ,pred, labels[np.argmax(pred)])
+
         return redirect("/")
 
-    return redirect("/", code=302)
+    return render_template("result.html", subgenre=winner)
+    
+    # return render_template("result.html", subgenre=pred)
 
 @app.route("/data", methods=["GET", "POST"])
 def data():
