6f29e51013e3b61638ec2e82c2f6cedc99a8c8af
Lisa Stroh <lisb020@yahoo.com>
2021-04-15 18:03:08 -0700

    preprocessing data

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from flask import (
     jsonify,
     request,
     redirect)
+from keras.preprocessing.text import Tokenizer as kerasTokenizer
+from keras.preprocessing.sequence import pad_sequences
+import pickle
 
 #################################################
 # Flask Setup
@@ -18,6 +21,17 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+def remove_nums(row):
+  output = ''.join(c for c in str(row) if not c.isdigit())
+  return(output)
+
+def remove_multi_spaces(row):
+  single_spaces = " ".join(str(row).split())
+  return(single_spaces)
+
+def remove_unicode(row):
+  string_encode = str(row).encode("ascii", "ignore")
+  return(string_encode.decode())
 
 # Send the user inputted data to the saved model
 @app.route("/send", methods=["GET", "POST"])
@@ -25,13 +39,23 @@ def send():
     if request.method == "POST":
         #bring in user input
         data = request.get_json()
-        print("hello",data)
+        print("data ",data)
+        data = data.lower()
+        data = remove_nums(data)
+        data = remove_multi_spaces(data)
+        data = remove_unicode(data)
+        data = [data]
+        
+        with open('tokenizer.pickle', 'rb') as handle:
+            tokenizer = pickle.load(handle)
+            seq = tokenizer.texts_to_sequences(data)
+            padded = pad_sequences(seq, maxlen=250)
+
         # Load the model
-        # from tensorflow.keras.models import load_model
-        # rating_model = load_model("rating_model.h5")
-        # description = request.form['']
+        from tensorflow.keras.models import load_model
+        # subgenre_model = load_model("subgenre_trained.h5")
         # model_loss, model_accuracy = rating_model.evaluate(description)
-        return redirect("/", data)
+        return redirect("/")
 
     return redirect("/", code=302)
 
