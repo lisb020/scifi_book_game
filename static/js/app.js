15a04c4e459ecc784159f1c957f6d35ad327b99a
Lisa Stroh <lisb020@yahoo.com>
2021-04-17 10:21:39 -0700

    flask app correct

--- static/js/app.js
+++ static/js/app.js
@@ -1,29 +1,19 @@
 $('body').on('click', '.rate', function(event){
     let description = $("#description").val();
-    // alert(description);
-    //trigger function to remove from database
+  
     const url = "/send"
-
-    fetch(url, {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(description)
-    })
-    .then(window.location("/"));
-    //event.preventDefault();
-    // desc = {"description":description}
-    //   $.ajax({
-    //     type:'POST',
-    //     url:url,
-    //     data: {description: description},
-    //     contentType: 'application/json; charset=utf-8',
-    //     dataType: 'json'
-    //     // success:function(response)
-    //     // {
-    //     //   window.location("/");
-    //     // }
-    //   });
-    
+    body_data = {"description":description}
+    console.log(body_data)
+  
+    axios({
+      method: 'post',
+      url: url,
+      data: body_data
+    }).then(response => {
+      answer = response.data.body
+      $('#subgenreRating').fadeOut(500, function() {
+        $(this).html("<h1> Your book belongs in the " + answer + " subgenre </h1>").fadeIn(500);
+      });
+      console.log(answer)
+    });
 });
